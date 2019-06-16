e45d3755d7bf6f6f91daec3764a7e6034118bead
berryrock <berry.rck@gmail.com>
2019-06-16 16:21:58 +0300

    Add main page

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Page
 
-def main_page(request, pk):
-	page = get_object_or_404(Page, pk=pk)
+def main_page(request):
+	page = get_object_or_404(Page)
 	return render(request, 'pages/main_page.html', {'page': page})
